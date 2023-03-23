1396b95be8b12ceed492942ed6383e993a7495c0
jeremysuau <jeremysuau@hotmail.fr>
2023-03-23 18:38:39 +0100

    Base du Character controller et map
    ajout d'un prototype de map
    ajout des mouvement du personnage (à revoir au complet) + animation + camera 3e personne fonctionnel

---
+++ Assets/PlayeInputs.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayeInputs : MonoBehaviour
+{
+	private PlayerInput playerInput;
+	private PlayerInputActions playerInputActions;
+
+	public float inputX;
+	public float inputY;
+	public bool inputJump;
+
+	private void Awake()
+	{
+		playerInput = GetComponent<PlayerInput>();
+		playerInputActions = new PlayerInputActions();
+		playerInputActions.Enable();
+	}
+
+	private void Update()
+	{
+		inputX = playerInputActions.Player.Move.ReadValue<Vector2>().x;
+		inputY = playerInputActions.Player.Move.ReadValue<Vector2>().y;
+		inputJump = playerInputActions.Player.Jump.triggered;
+	}
+}
